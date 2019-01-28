9065ad0e6ea23dabbbfd4f2af387aa409e8f60d6
huongcm1 <huongcm1@smartosc.com>
2019-01-29 01:48:49 +0700

    create category page(list + grid)

---
+++ scripts/category.js
@@ -0,0 +1,19 @@
+$(':radio').change(function() {
+  console.log('New star rating: ' + this.value);
+});
+
+$('.bottom__item').hover(function(){
+    var datasrc = $(this).attr("data-src");
+    $(this).parent().siblings().find("img").attr("src",datasrc);
+    $(this).siblings().removeClass("active");
+    $(this).addClass("active");
+}) 
+
+$('.grid__item').on('click', function(e){
+    var data = $(this).attr("data");
+    $(this).siblings().removeClass("active");
+    $(this).addClass("active");
+    var productLayout = $(".grid").find("[id='" + data + "']");
+    productLayout.siblings().removeClass("active");
+    productLayout.addClass("active");  
+})
