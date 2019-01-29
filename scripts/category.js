d2f0407bfa1eeff9035fba01971c57d8be1e8d5f
huongcm1 <huongcm1@smartosc.com>
2019-01-29 18:45:13 +0700

    update list+grid page

--- scripts/category.js
+++ scripts/category.js
@@ -13,3 +13,9 @@ $('.grid__item').on('click', function(e){
     productLayout.siblings().removeClass("active");
     productLayout.addClass("active");  
 })
+
+$(function() {
+    $("#price-range").slider({range: true, min: 0.00, max: 500.00, values: [0.00, 500.00], slide: function(event, ui) {$("#priceRange").val("$" + ui.values[0]+ ".00" + " - $" + ui.values[1] + ".00");}
+    });
+    $("#priceRange").val("$" + $("#price-range").slider("values", 0) + ".00" + " - $" + $("#price-range").slider("values", 1) + ".00");
+  });
