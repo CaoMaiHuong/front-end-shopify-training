fcc14f51e3cf8f76d0702a96198d3f7004d8dbd2
huongcm1 <huongcm1@smartosc.com>
2019-01-30 14:23:45 +0700

    complete category page

--- scripts/category.js
+++ scripts/category.js
@@ -5,14 +5,31 @@ $('.bottom__item').hover(function(){
     $(this).addClass("active");
 }) 
 
-$('.grid__item').on('click', function(e){
-    var data = $(this).attr("data");
-    $(this).siblings().removeClass("active");
-    $(this).addClass("active");
-    var productLayout = $(".grid").find("[id='" + data + "']");
-    productLayout.siblings().removeClass("active");
-    productLayout.addClass("active");  
-})
+// $('.grid__item').on('click', function(e){
+//     var data = $(this).attr("data");
+//     $(this).siblings().removeClass("active");
+//     $(this).addClass("active");
+//     var productLayout = $(".grid").find("[id='" + data + "']");
+//     productLayout.siblings().removeClass("active");
+//     productLayout.addClass("active");  
+// })
+    var buttonGrid = $('.view-mode .view-grid'),
+    buttonList = $('.view-mode .view-list'),
+    productList = $('.product-list');
+    buttonGrid.on('click', function(e) {
+    	
+    	$(this).addClass('active');
+    	buttonList.removeClass('active');
+    	productList.removeClass('product-view-list');
+		productList.addClass('product-view-grid');
+    });
+    buttonList.on('click', function(e) {
+        e.preventDefault();
+        $(this).addClass('active');
+        buttonGrid.removeClass('active');
+        productList.removeClass('product-view-grid');
+        productList.addClass('product-view-list');
+    });
 
 $(function() {
     $("#price-range").slider({range: true, min: 0.00, max: 500.00, values: [0.00, 500.00], slide: function(event, ui) {$("#priceRange").val("$" + ui.values[0]+ ".00" + " - $" + ui.values[1] + ".00");}
