0bdc16e0fcc8db92ee24995e9ccfd811f9e1a4c3
huongcm1 <huongcm1@smartosc.com>
2019-01-31 19:08:17 +0700

    complete detail page

--- scripts/detail.js
+++ scripts/detail.js
@@ -10,3 +10,46 @@ $('.bottom__item').hover(function(){
     $(this).siblings().removeClass("active");
     $(this).addClass("active");
 })  
+
+
+$('.main-product__detail__color ul .color').on('click', function(){
+    $(this).addClass('active');
+    $(this).siblings().removeClass('active');
+})
+
+$('.main-product__detail__add__quantity span.plus').on('click', function(){
+    var value = $('.main-product__detail__add__quantity input').attr('value');
+    value++;
+    $('.main-product__detail__add__quantity input').attr("value",value);
+})
+$('.main-product__detail__add__quantity span.minus').on('click', function(){
+    var value = $('.main-product__detail__add__quantity input').attr('value');
+    if(value<=1){
+        value=1;
+    }
+    else{
+        value--;
+    }
+    $('.main-product__detail__add__quantity input').attr("value",value);
+})
+
+var img = $('.main-product__list__slider__image'),
+    height = img.height();
+
+$('.arrow').on('click', function(){
+    $(this).addClass('active');
+    $(this).siblings().removeClass('active');
+    var up = $(this).is('.up');
+    if (up) {
+        img.animate({'scrollTop': '-=' + 135});
+    } else {
+        img.animate({'scrollTop': '+=' + 135});        
+    }
+});
+$('.main-product__slider__item').on('click', function(){
+    $(this).addClass('active');
+    $(this).siblings().removeClass('active');
+    var src = $(this).find('img').attr('src');
+    $('.main-product__big-image img').attr('src',src);
+});
+
