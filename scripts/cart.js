b2b87813222b899758490075211f3649eb40612a
huongcm1 <huongcm1@smartosc.com>
2019-02-01 12:20:06 +0700

    add cart page

---
+++ scripts/cart.js
@@ -0,0 +1,17 @@
+$('.cart__product__item .cart__product__quantity span.plus').on('click', function(){
+    var ip = $(this).siblings().find('input');
+    var value= ip.attr('value');
+    value++;
+    ip.attr('value',value);
+})
+$('.cart__product__quantity span.minus').on('click', function(){
+    var ip = $(this).siblings().find('input');
+    var value= ip.attr('value');
+    if(value<=1){
+        value=1;
+    }
+    else{
+        value--;
+    }
+    ip.attr('value',value);
+})
