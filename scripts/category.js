7d52e76fb615af860b878b4f35130a679f95b33f
huongcm1 <huongcm1@smartosc.com>
2019-02-13 11:16:32 +0700

    update responsive category page

--- scripts/category.js
+++ scripts/category.js
@@ -21,10 +21,12 @@ buttonList.on('click', function(e) {
     buttonGrid.removeClass('active');
     productList.removeClass('product-view-grid');
     productList.addClass('product-view-list');
+    $('.item').removeClass('col-xl-4 col-lg-6 col-md-4 col-sm-6 col-xs-6');
+    $('.item').addClass('collumn');
 });
 
 $(function() {
-    $("#price-range").slider({range: true, min: 0.00, max: 500.00, values: [0.00, 500.00], slide: function(event, ui) {$("#priceRange").val("$" + ui.values[0]+ ".00" + " - $" + ui.values[1] + ".00");}
+    $("#price-range").slider({range: true, min: 0.00, max: 500.00, values: [0.00, 500.00], slide: function(event, ui) {$("#priceRange").val("$" + ui.values[0] + " - $" + ui.values[1]);}
     });
-    $("#priceRange").val("$" + $("#price-range").slider("values", 0) + ".00" + " - $" + $("#price-range").slider("values", 1) + ".00");
+    $("#priceRange").val("$" + $("#price-range").slider("values", 0) + " - $" + $("#price-range").slider("values", 1));
   });
